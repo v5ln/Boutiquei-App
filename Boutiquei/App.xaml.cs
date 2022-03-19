b9bbc41e33a21d2326194f3bcc7ea4a4d6542bab
OMAR <v5ln@icloud.com>
2022-03-20 01:55:30 +0200

    temp files

--- Boutiquei/App.xaml.cs
+++ Boutiquei/App.xaml.cs
@@ -28,5 +28,57 @@ namespace Boutiquei
         protected override void OnResume()
         {
         }
+
+        // public async Task<List<BoutiqueiModel>> GetAll()
+        // {
+
+        //   return (await firebaseClient
+        //  .Child("Stores").OnceAsync<BoutiqueiModel>()).Select(item => 
+        //       {
+
+        //           Id = item.Key,
+        //           BName = item.Object.BName,
+        //           BMainPic = item.Object.BMainPic,
+        //           Type = item.Object.Type,
+        //           BCoverPic = item.Object.BCoverPic,
+        //           //
+
+
+        //       })
+        //       .Select(Item.Object.Colors =>  )
+        //       .Where(s => s.Type == "Boutique").ToList(); 
+
+
+
+        // }
+        // public async Task<List<String>> GetAll()
+        // {
+
+        //   return (await firebaseClient
+        //  .Child("Stores/color").OnceAsync<<List<String>>()).Select(item => new List<String>
+        //       {
+
+        //           colors.append(item)
+
+
+        //       })
+        //       .Select(Item.Object.Colors =>  )
+        //       .Where(s => s.Type == "Boutique").ToList(); // [gr]
+        // }
+
+
+        // p1
+
+        // Color : 
+        // list<string> colors;
+
+        
+        // Product{
+        // public String Name = "p1";
+        // public List<String> Color = ["green","blue"];
+
+        // }
+
+
     }
 }
