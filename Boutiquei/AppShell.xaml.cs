1c43547e331694a3f15ad53da36ec7138d39c7e1
OMAR <v5ln@icloud.com>
2022-03-19 19:32:29 +0200

    init mvvm

---
+++ Boutiquei/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Boutiquei.ViewModels;
+using Boutiquei.Views;
+using Xamarin.Forms;
+
+namespace Boutiquei
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Boutiquei/Views/BrandsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Boutiquei.Views
+{
+    public partial class BrandsPage : ContentPage
+    {
+        public BrandsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
