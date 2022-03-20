df8a8501c72897a7ab42b9b5b98a05e2734bba66
eleen <dbarahma8@gmail.com>
2022-03-20 03:07:33 +0300

    Add Models

---
+++ Boutiquei/Models/StoreModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boutiquei.Models
+{
+    class StoreModel
+    {
+
+        public string Id { set; get; }
+
+        public string BMainPic { set; get; }
+
+        public string BCoverPic { set; get; }
+
+        public string BName { set; get; }
+        public string Type { set; get; }
+
+    }
+}
