2ffd9d884ba8c05f05ccaf061b42566a6e4c5c6e
OMAR <v5ln@icloud.com>
2022-04-03 21:39:46 +0300

    add cart dummy data

--- Boutiquei/Boutiquei/Models/UserProduct.cs
+++ Boutiquei/Boutiquei/Models/UserProduct.cs
@@ -3,8 +3,16 @@ namespace Boutiquei.Models
 {
     public class UserProduct
     {
-        public UserProduct()
-        {
-        }
+        public string PID { set; get; }
+
+        public string PName { set; get; }
+
+        public string Price { set; get; }
+
+        public string UserID { set; get; }
+
+        public int Quantity { set; get; }
+
+        public string ProductImage { set; get; }
     }
 }
