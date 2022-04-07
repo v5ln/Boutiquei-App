cb61ada15b56cf936c29dfd6646400301fa8081a
Farahalmieh <v5ln@icloud.com>
2022-04-07 04:44:53 +0300

    Sprint-4 Files

---
+++ Boutiquei/Boutiquei/Views/CheckoutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Boutiquei.Views
+{
+    public partial class CheckoutPage : ContentPage
+    {
+        public CheckoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
