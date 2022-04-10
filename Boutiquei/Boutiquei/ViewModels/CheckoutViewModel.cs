1c4402e076c3dda8c596f4676576a3a34193ef72
Omar Al-Banna <v5ln@icloud.com>
2022-04-11 00:48:40 +0300

    check out dummy data

---
+++ Boutiquei/Boutiquei/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Boutiquei.Models;
+using MvvmHelpers;
+
+namespace Boutiquei.ViewModels
+{
+    public class CheckoutViewModel : BaseViewModel
+    {
+        //public Order Order { get; set; }
+        public Address Address;
+        public CheckoutViewModel()
+        {
+            //Order = new Order();
+            Address = new Address { AddressDetails = "Faisal Street", City = "Nablus", Name = "Omar", Phone = "065316372", District = "Downtown" };
+
+        }
+    }
+}
