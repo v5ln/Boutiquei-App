c8196338a86e5d2527b373f5a67d629d5cdd84c0
OMAR <v5ln@icloud.com>
2022-04-13 01:55:26 +0300

    Setup Android Auth

---
+++ Boutiquei/Boutiquei.Android/AndriodAuth.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Foundation;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndriodAuth))]
+namespace Boutiquei.Droid
+{
+    public class AndriodAuth : IGoogleAuth
+    {
+        public bool IsSigIn()
+        {
+            var user = FirebaseAuth.Instance.CurrentUser;
+            return user != null;
+
+        }
+
+        public bool IsSigOut()
+        {
+            try
+            {
+                FirebaseAuth.Instance.SignOut();
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+
+            }
+
+        }
+
+        public async Task<string> LoginWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var user = await Firebase.Auth.FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+
+                var token = user.User.Uid;
+
+                return token.ToString();
+            }
+            catch (FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+        }
+
+        public async Task<string> SignUpWithEmailAndPassword(string email, string password)
+        {
+
+
+            try
+            {
+
+                var auth = await FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+                var token = auth.User.Uid;
+
+                return token.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
